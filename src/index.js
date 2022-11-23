c67d4e9df64e57771bf47eef7d58564b8c2f61ef
Marc Rufeis <github@craftycram.net>
2022-11-23 15:51:20 +0100

    add code

---
+++ src/index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const Twit = require('twit');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const Twitter = new Twit({
+  consumer_key:         process.env.API_KEY,
+  consumer_secret:      process.env.API_SECRET,
+  app_only_auth:        true
+})
+
+let currentTweet = {};
+
+function getCurrentTweet() {
+    const params = {
+      q: 'from:NetworkChuck',
+      result_type: 'recent',
+      count: 1
+  } 
+
+    Twitter.get('search/tweets', params, (err, data, _response) => {
+        const tweets = data.statuses
+
+        if (!err) {
+          
+          currentTweet = tweets[0];
+
+        }
+    })
+}
+
+getCurrentTweet();
+setInterval(getCurrentTweet, 30 * 1000);
+
+app.get('/', (_req, res) => {
+  res.status(200).send(currentTweet);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
